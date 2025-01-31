ac5c8a0e5b1bf1ae8e703e790052c2011a0f25ce
Itztiva <tiva@Itztiva.com>
2025-01-31 18:38:19 -0500

    remvoe unneeded string

--- Source/Globals.h
+++ Source/Globals.h
@@ -9,7 +9,6 @@ namespace Paradise {
     namespace Strings {
         constexpr auto ProcessRequest = L"Could not set libcurl options for easy handle, processing HTTP request failed. Increase verbosity for additional information.";
         constexpr auto ProcessRequestStat = L"STAT_FCurlHttpRequest_ProcessRequest.";
-        constexpr auto ProcessRequestFail = L"ProcessRequest failed. URL '%s' is not a valid HTTP request. %p";
         constexpr auto Realloc = L"AbilitySystem.Debug.NextTarget";
     }
 }
