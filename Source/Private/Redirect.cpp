06cf06b9d37969f192ae2a502feb8129f398ae5a
Itztiva <tiva@Itztiva.com>
2025-01-31 18:24:54 -0500

    make this look better

--- Source/Private/Redirect.cpp
+++ Source/Private/Redirect.cpp
@@ -1,28 +1,31 @@
 #include "pch.h"
 #include "Redirect.h"
 
-bool Paradise::Redirect::ProcessRequest(FCurlHttpRequest* Request)
-{
-    const std::wstring EPICDOMAINS[] = {
-        L"ol.epicgames.com",
-        L"ol.epicgames.net",
-        L"on.epicgames.com",
-        L"game-social.epicgames.com",
-        L"ak.epicgames.com"
+bool Paradise::Redirect::ProcessRequest(FCurlHttpRequest* Request) 
+{   
+    auto check = [](const std::wstring& fullUrl) -> std::pair<bool, size_t> {
+        const auto domains = {
+            std::wstring(L"game-social.epicgames.com"),
+            std::wstring(L"ol.epicgames.com"),
+            std::wstring(L"ol.epicgames.net"),
+            std::wstring(L"on.epicgames.com"), 
+            std::wstring(L"ak.epicgames.com")
+        };
+
+        for (const auto& d : domains) {
+            if (size_t pos = fullUrl.find(d); pos != std::wstring::npos) {
+                return {true, pos + d.length()};
+            }
+        }
+        return {false, 0};
     };
 
-    std::wstring URL(Request->GetURL().ToString());
+    std::wstring URL = Request->GetURL().ToString();
     
-    for (const auto& domain : EPICDOMAINS)
-    {
-        size_t PathIndex = URL.find(domain);
-        if (PathIndex != std::wstring::npos)
-        {
-            auto Path = URL.substr(PathIndex + domain.length());
-            auto NewURL = Paradise::BACKEND_URL + Path;
-            Request->SetURL(NewURL.c_str());
-            break;
-        }
+    if (auto [found, end] = check(URL); found) {
+        std::wstring newPath = URL.substr(end);
+        std::wstring newUrl = Paradise::BACKEND_URL + newPath;
+        Request->SetURL(newUrl.c_str());
     }
     
     return ProcessRequestOG(Request);
