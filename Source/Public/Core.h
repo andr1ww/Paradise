c734d1ed2acacead7aafc42de9c62f33acd852cb
Itztiva <tiva@Itztiva.com>
2025-08-14 17:37:20 -0400

    open sourced guys // thx to ploosh for contributions

--- Source/Public/Core.h
+++ Source/Public/Core.h
@@ -1,7 +1,24 @@
 #pragma once
 #include "pch.h"
+#include <detours.h>
 
-namespace Paradise::Core 
+namespace Paradise::Core
 {
     void Initialize();
+
+    inline uintptr_t Addr = 0;
+
+    static int index = 0;
+    inline int64_t PakPatch(int64_t a1, int64_t a2, int64_t a3, int64_t a4, int64_t a5, int64_t a6)
+    {
+        index++;
+        *(DWORD*)a1 = 0;
+        if (index == 27) { // 27 is 14.40 but it may be 30 for 15.30
+            DetourTransactionBegin();
+            DetourUpdateThread(GetCurrentThread());
+            DetourDetach(&(PVOID&)Addr, (PVOID)PakPatch);
+            DetourTransactionCommit();
+            return a1;
+        }
+    }
 }
--- dllmain.cpp
+++ dllmain.cpp
@@ -2,9 +2,32 @@
 #include <Windows.h>
 #include "Source/Public/Core.h"
 #include <iostream>
+#include <detours.h>
 
 DWORD WINAPI Main(LPVOID)
 {
+    if (Paradise::SIG_PATCH) {
+        const char* sigs[] = { // idk which is for which ver so + ts is easier if i find more sigs
+            "40 55 53 56 57 41 56 41 57 48 8D 6C 24 ? 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 45 ? 4D 8B F1", 
+            "40 55 53 56 57 41 56 41 57 48 8D 6C 24 ? 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 45 ? 45 33 FF 49 8B F1"
+        };
+
+        Paradise::Core::Addr = 0;
+
+        for (const char* sig : sigs) {
+            Paradise::Core::Addr = Memcury::Scanner::FindPattern(sig).Get();
+            if (Paradise::Core::Addr)
+                break;
+        }
+
+        if (Paradise::Core::Addr) {
+            DetourTransactionBegin();
+            DetourUpdateThread(GetCurrentThread());
+            DetourAttach(&(PVOID&)Paradise::Core::Addr, (PVOID)Paradise::Core::PakPatch);
+            DetourTransactionCommit();
+        }
+    }
+
     if (Paradise::SHOW_CONSOLE) {
         AllocConsole();
         FILE* fptr;
@@ -45,6 +68,7 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     {
     case DLL_PROCESS_ATTACH:
 		CreateThread(0, 0, (LPTHREAD_START_ROUTINE)Main, 0, 0, 0);
+        break;
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
     case DLL_PROCESS_DETACH:
@@ -52,4 +76,3 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     }
     return TRUE;
 }
-
