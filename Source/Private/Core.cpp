30df69abd58ec6f23f471ecfd2071fd8470d4d59
andr1ww <tiva@itztiva.com>
2025-11-22 08:38:00 -0500

    fix lots of versions

--- Source/Private/Core.cpp
+++ Source/Private/Core.cpp
@@ -4,65 +4,76 @@
 #include "Finder.h"
 #include <detours.h>
 
-namespace Paradise 
+namespace Paradise
 {
-    void Core::Initialize() {
-        const std::vector<uint8_t> PATTERN_1 = { 0x48, 0x81, 0xEC };
-        const std::vector<uint8_t> PATTERN_2 = { 0x40 };
-        const std::vector<uint8_t> PATTERN_3 = { 0x48, 0x8B, 0xC4 };
-		const std::vector<uint8_t> PATTERN_4 = { 0x4C, 0x8B, 0xDC };
-        const std::vector<uint8_t> PATTERN_5 = { 0x48, 0x83, 0xEC };
+    void Core::Initialize()
+    {
+        const std::vector<uint8_t> PATTERN_1 = {0x48, 0x81, 0xEC};
+        const std::vector<uint8_t> PATTERN_2 = {0x40};
+        const std::vector<uint8_t> PATTERN_3 = {0x48, 0x8B, 0xC4};
+        const std::vector<uint8_t> PATTERN_4 = {0x4C, 0x8B, 0xDC};
+        const std::vector<uint8_t> PATTERN_5 = {0x48, 0x83, 0xEC};
 
         auto scanner = Memcury::Scanner::FindStringRef(Strings::ProcessRequestStat, Memcury::PE::GetModuleBase(), false);
 
-        if (!scanner.IsValid()) {
+        if (!scanner.IsValid())
+        {
             scanner = Memcury::Scanner::FindStringRef(Strings::ProcessRequest);
             ProcessRequestOG = scanner
-                .ScanFor(PATTERN_1, false)
-                .ScanFor(PATTERN_2, false)
-                .GetAs<decltype(ProcessRequestOG)>();
+                                   .ScanFor(PATTERN_1, false)
+                                   .ScanFor(PATTERN_2, false)
+                                   .GetAs<decltype(ProcessRequestOG)>();
         }
-        else {
+        else
+        {
             ProcessRequestOG = scanner
-                .ScanFor(PATTERN_1, false)
-                .ScanFor(PATTERN_2, false)
-                .GetAs<decltype(ProcessRequestOG)>();
+                                   .ScanFor(PATTERN_1, false)
+                                   .ScanFor(PATTERN_2, false)
+                                   .GetAs<decltype(ProcessRequestOG)>();
         }
 
-        if (auto targetPtr = ProcessRequestOG) {
-            auto ref = Memcury::Scanner::FindPointerRef(targetPtr).GetAs<void**>();
-            if (!ref) {
-                if (scanner.IsValid()) {
+        if (auto targetPtr = ProcessRequestOG)
+        {
+            auto ref = Memcury::Scanner::FindPtrRef(targetPtr).GetAs<void **>();
+            if (!ref)
+            {
+                if (scanner.IsValid())
+                {
                     ProcessRequestOG = scanner
-                        .ScanFor(PATTERN_5, false)
-						.ScanFor(PATTERN_2, false)
-                        .GetAs<decltype(ProcessRequestOG)>();
-                    ref = Memcury::Scanner::FindPointerRef(targetPtr).GetAs<void**>();
-                    if (!ref) {
+                                           .ScanFor(PATTERN_5, false)
+                                           .ScanFor(PATTERN_2, false)
+                                           .GetAs<decltype(ProcessRequestOG)>();
+                    ref = Memcury::Scanner::FindPtrRef(targetPtr).GetAs<void **>();
+                    if (!ref)
+                    {
                         ProcessRequestOG = scanner
-                            .ScanFor(PATTERN_3, false)
-                            .GetAs<decltype(ProcessRequestOG)>();
-                        ref = Memcury::Scanner::FindPointerRef(targetPtr).GetAs<void**>();
-                        if (!ref) {
+                                               .ScanFor(PATTERN_3, false)
+                                               .GetAs<decltype(ProcessRequestOG)>();
+                        ref = Memcury::Scanner::FindPtrRef(targetPtr).GetAs<void **>();
+                        if (!ref)
+                        {
                             ProcessRequestOG = scanner
-                                .ScanFor(PATTERN_4, false)
-                                .GetAs<decltype(ProcessRequestOG)>();
+                                                   .ScanFor(PATTERN_4, false)
+                                                   .GetAs<decltype(ProcessRequestOG)>();
                         }
                     }
                 }
             }
 
-            if (ref) {
+            if (ref)
+            {
                 DWORD oldProtect;
-                if (VirtualProtect(ref, sizeof(void*), PAGE_EXECUTE_READWRITE, &oldProtect)) {
-                    *ref = reinterpret_cast<void*>(Redirect::ProcessRequest);
-                    VirtualProtect(ref, sizeof(void*), oldProtect, &oldProtect);
+                if (VirtualProtect(ref, sizeof(void *), PAGE_EXECUTE_READWRITE, &oldProtect))
+                {
+                    *ref = reinterpret_cast<void *>(Redirect::ProcessRequest);
+                    VirtualProtect(ref, sizeof(void *), oldProtect, &oldProtect);
                     Log("Successfully hooked ProcessRequest");
                 }
             }
         }
-        else {
-            *Memcury::Scanner::FindPointerRef(targetPtr).GetAs<void**>() = Redirect::ProcessRequest;
+        else
+        {
+            *Memcury::Scanner::FindPtrRef(targetPtr).GetAs<void **>() = Redirect::ProcessRequest;
             Paradise::Finder::InitializeExitHook();
             Log("Error: Failed to find ProcessRequest");
         }
